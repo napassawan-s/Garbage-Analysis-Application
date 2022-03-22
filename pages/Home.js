1ab1b3d38b73b37b7e3fbd124f10fd81199ea3f4
Napassawan Subthawetheerakul <sc21n2s@leeds.ac.uk>
2022-03-22 18:42:02 +0000

    set up App.js and add pages and component folders

---
+++ pages/Home.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Home = ({ navigation }) => {
+    return(
+        <Text>Garbage Analysis Application</Text>
+    );
+
+}
+
+export default Home;
