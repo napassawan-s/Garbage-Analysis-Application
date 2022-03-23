1b3e88f71e6af2ff0e30474ad14375017c09ccb5
Napassawan Subthawetheerakul <sc21n2s@leeds.ac.uk>
2022-03-23 23:18:56 +0000

    edit ResultPanel and Header

--- components/ResultPanel.js
+++ components/ResultPanel.js
@@ -1,24 +1,58 @@
 import React from 'react';
-import { Card } from 'react-native-elements';
-import { Text, View, StyleSheet, Image} from 'react-native';
+import { Card, Header } from 'react-native-elements';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 //import { Text } from './Text';
 
 const ResultPanel = (props, { navigation }) => {
     return (
-    <Card containerStyle={{ width: '95%', borderStyle: 'none' }}>
-        <Card.Title style={styles.cardTitle}>GLASS</Card.Title>
-        <Card.Divider />
-        <Image
-          source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}} style={{width: 50, height: 50}}
-        />
-    </Card>
+        
+            <Card containerStyle={{
+                width: '95%', backgroundColor: 'white',
+                borderWidth: 0,
+                shadowColor: 'white',
+                justifyContent: 'center'
+
+            }}
+            >
+                <Card.Title style={styles.cardTitle}>{props.data['title']}</Card.Title>
+                <Card.Divider />
+                <View style={styles.imgContainer}>
+                    {
+                        props.data['uri'].map((pic, index) => {
+                            return (
+                                <Image
+                                    key={index}
+                                    source={{ uri: pic }}
+                                    style={styles.img}
+                                />
+                            );
+                        })
+                    }
+                </View>
+            </Card>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     cardTitle: {
-        textAlign: 'left'
-    }
+        textAlign: 'left',
+        fontFamily: 'Montserrat-Bold',
+        fontSize: 17
+    },
+    img: {
+        width: 50,
+        height: 50,
+        margin: 4
+    },
+    imgContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+   
 });
 
 export default ResultPanel;
